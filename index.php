604ecd3c63371eb16fdb0ac22b9009fe230373d4
ederant <99449142+ederant@users.noreply.github.com>
2024-06-16 20:52:03 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Bienvenido a SimplePHPMvcOOP</h1>";
+echo "<p>Este es un ejemplo del proyecto corriendo en tu máquina.</p>";
+echo "<p>Repositorio: <a href='https://github.com/ederant/negociosweb'>https://github.com/ederant/negociosweb</a></p>";
+?>
